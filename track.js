64ef9efd57c48f984cdae8c0d9ea979955f70cc5
Oliver Roßhoff <oliver.rosshoff@gmail.com>
2022-06-30 09:27:33 +0200

    test

--- track.js
+++ track.js
@@ -1,27 +1,7 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const testButton = document.querySelector("#testButton");
 
-// app.get /
-//   ("/",
-//   (req, res) => {
-//     res.send('{message: "Hello World" }');
-//   });
+testButton.addEventListener("click", buttonClick());
 
-app.listen(port, () => {
-  //console.log("go to http://localhost:3000/");
-});
-
-console.log("test222");
-
-/* loadTodos();
-
-function loadTodos() {
-  fetch("https://opensky-network.org/api")
-    .then((res) => res.json())
-    .then((todosFromApi) => {
-      todos = todosFromApi;
-      console.log(todosFromApi + "test 731");
-    });
+function buttonClick() {
+  console.log("Hallo Welt");
 }
-*/
