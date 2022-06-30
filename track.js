aeade3835ba0a1d43898909ebd04c3b274e4a26e
Oliver Roßhoff <oliver.rosshoff@gmail.com>
2022-06-30 10:06:31 +0200

    test15

--- track.js
+++ track.js
@@ -2,8 +2,15 @@ const testButton = document.querySelector("#testButton");
 
 testButton.addEventListener("click", function test2() {
   console.log("test3");
+  loadTodos();
+  console.log("test17");
 });
 
-function testFunction() {
-  console.log("test4");
+function loadTodos() {
+  fetch("https://opensky-network.org/api")
+    .then((res) => res.json())
+    .then((todosFromApi) => {
+      todos = todosFromApi;
+      console.log(todosFromApi + "test 731");
+    });
 }
